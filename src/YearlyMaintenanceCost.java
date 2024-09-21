9173c78d73737c2f3efe204d58cecd8dcaad38ba
SalesTax <masseyas@mail.uc.edu>
2024-09-21 19:18:02 -0400

    Question 3

---
+++ src/YearlyMaintenanceCost.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class YearlyMaintenanceCost {
+    public static void main(String[] args)
+    {
+            double winterCost = 500;
+            double springCost = 300;
+            double summerCost = 400;
+            double fallCost = 350;
+            double totalCost = winterCost + springCost + summerCost + fallCost;
+            System.out.println("Yearly Maintenance Cost: $" + totalCost);
+    }
+}

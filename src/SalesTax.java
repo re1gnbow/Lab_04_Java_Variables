3216940e172440207edffb26aa449c82dd821fab
SalesTax <masseyas@mail.uc.edu>
2024-09-21 14:19:53 -0400

    init commit

---
+++ src/SalesTax.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class SalesTax {
+    public static void main(String[] args) {
+            double price = 100.00; // Price of purchase
+            double SalesTax = price*0.05; // 5% sales tax
+        System.out.printf("Price: $" + price);
+        System.out.println("Sales Tax: $" + SalesTax);
+        }
+    }

b731388f5680016a3768f2937673563e97a18cd4
SalesTax <masseyas@mail.uc.edu>
2024-09-21 19:39:40 -0400

    Question 4

---
+++ src/EvenOdd.java
@@ -0,0 +1,13 @@
+
+public class EvenOdd {
+    public static void main(String[] args)
+    {
+        int NumToExamine = 7;
+        if (NumToExamine % 2 == 0) {
+
+            System.out.println(NumToExamine + " is even.");
+        } else {
+            System.out.println(NumToExamine + " is odd.");
+        }
+    }
+}
